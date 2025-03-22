ba8a6fd99073921a9c06b7b7ce14351ac1465256
JNANISMART <125004107@sastra.ac.in>
2025-03-22 21:52:29 +0530

    Initial Commit

---
+++ Clippy/ClippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippyApp.swift
+//  Clippy
+//
+//  Created by Jnani Smart on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ClippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClippyTests/ClippyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClippyTests.swift
+//  ClippyTests
+//
+//  Created by Jnani Smart on 22/03/25.
+//
+
+import Testing
+@testable import Clippy
+
+struct ClippyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
